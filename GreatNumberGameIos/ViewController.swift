98e57c48fe058e1ef5dc0339caef5930bfa1f68d
fatengh <faten.arg@hotmail.com>
2021-12-20 22:40:31 +0000

    first commit

--- GreatNumberGameIos/ViewController.swift
+++ GreatNumberGameIos/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+       
+       @IBOutlet weak var numberTextField: UITextField!
+       var num : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        num = Int.random(in: 1...100)
+        print(num)
+       }
+    
+    @IBAction func submitPressed(_ sender: UIButton) {
+        
+        
+        let x = Int(numberTextField.text ?? "0")
+        if x! > num{
+            alertincorect("\(x!) is too High")
+        }else if x! < num{
+            alertincorect("\(x!) is too Low")
+        }else{
+            alertcorect("\(x!) is Correct")
+        }
+        numberTextField.text = ""
+        
     }
 
-
-}
-
+        
+        func alertcorect( _ message: String){
+                let alertDailog = UIAlertController(title: "correct", message: message, preferredStyle: UIAlertController.Style.alert)
+                alertDailog.addAction(UIAlertAction(title: "Play Again", style: UIAlertAction.Style.cancel, handler: { _ in
+                    self.num = Int.random(in: 1...100)
+                }))
+                alertDailog.view.subviews.first?.subviews.first?.subviews.first?.backgroundColor = UIColor.green
+                alertDailog.view.tintColor = UIColor.white
+                self.present(alertDailog, animated: true, completion: nil)
+            
+        }
+    
+      func  alertincorect( _ message: String){
+           let alertDailog = UIAlertController(title: "Incorrect", message: message, preferredStyle: UIAlertController.Style.alert)
+           alertDailog.addAction(UIAlertAction(title: "Guess Again", style: UIAlertAction.Style.cancel, handler: { _ in
+            }))
+          alertDailog.view.subviews.first?.subviews.first?.subviews.first?.backgroundColor = UIColor.red
+        
+          self.present(alertDailog, animated: true, completion: nil)
+    }
+    }
